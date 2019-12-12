965cad66a0804e610550be3aa86f1b53ec8c652b
Gustavo <gustavolmourao@gmail.com>
2019-12-12 13:28:29 -0300

    update info; move files

---
+++ test_audio_enc.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class TestAudioEncoder(unittest.TestCase):
+    """
+    Class responsible to call unit test methods of Processor class.
+    """
+    def test_decoded_image(self):
+        """
+        """
+
+
+if __name__ == "__main__":
+    """
+    Call unit test methods.
+    """
+    unittest.main()
